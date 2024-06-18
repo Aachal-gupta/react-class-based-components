5b55bf624b376281d0c1d59f4788ce090e65b0f6
Aachal-gupta <aachalgupta8080@gmail.com>
2024-06-18 17:30:20 +0530

    class-based-components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+   c="ghy";
+  render() {
+    return (
+      <div>
+        hello this is {this.c}
+      </div>
+    )
+  }
+}
+

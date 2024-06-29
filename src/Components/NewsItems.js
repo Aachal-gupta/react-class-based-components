4ef39365281f0bf8601e312aa0842e5ca5c052e9
Aachal-gupta <aachalgupta8080@gmail.com>
2024-06-30 00:03:21 +0530

    api fetch with next  button

--- src/Components/NewsItems.js
+++ src/Components/NewsItems.js
@@ -7,11 +7,11 @@ export class NewsItems extends Component {
     return (
       <div>
             <div className="card" style={{width: "18rem"}}>
-                <img src={imageUrl} className="card-img-top" alt="..."/>
+                <img src={!imageUrl? "https://ichef.bbci.co.uk/news/1024/branded_news/9111/live/73f7b260-350b-11ef-b606-993eb74348a6.jpg":imageUrl } className="card-img-top" alt="..."/>
                 <div className="card-body">
-                    <h5 className="card-title">{title}...</h5>
-                    <p className="card-text">{description}...</p>
-                  <a href={ newsUrl}  className="btn  btn-sm btn-primary">Read More</a>
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                  <a  rel="noreferrer"  href={ newsUrl}  className="btn  btn-sm btn-dark">Read More</a>
                 </div>
             </div>
       </div>
